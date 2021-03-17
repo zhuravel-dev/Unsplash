2e6750be042db11e3bd79a1b5704db82e62e4b8c
Yuliya Zhuravel <zhuravel.dev@gmail.com>
2021-03-17 16:01:43 +0200

    create RecyclerView

--- app/src/main/java/com/example/unsplash/MainActivity.kt
+++ app/src/main/java/com/example/unsplash/MainActivity.kt
@@ -3,13 +3,39 @@ package com.example.unsplash
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.unsplash.adapter.Adapter
 import com.unsplash.pickerandroid.photopicker.UnsplashPhotoPicker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myAdapter: Adapter
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+
+        myAdapter = Adapter(this)
+
+    }
+
+
+    fun setupRecyclerView(){
+        val myRecyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+        myRecyclerView.adapter = this.myAdapter
+
+
+
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/unsplash/adapter/Adapter.kt
+++ app/src/main/java/com/example/unsplash/adapter/Adapter.kt
@@ -1,10 +1,33 @@
 package com.example.unsplash.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.unsplash.R
+import com.example.unsplash.models.Results
 import com.unsplash.pickerandroid.photopicker.data.UnsplashPhoto
 
-class Adapter : RecyclerView.Adapter<>() {
 
-    private var ListOfPhotos : MutableList<UnsplashPhoto>  = mutableListOf()
+class Adapter constructor(context: Context): RecyclerView.Adapter<ViewHolder>() {
+
+    private var mListOfPhotos : MutableList<Results>  = mutableListOf()
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.item_photo, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int = mListOfPhotos.size
 
 }
